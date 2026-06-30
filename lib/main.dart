15329afe1688397aa1d9ff74479be58a42bbf08a
Your Name <you@example.com>
2026-06-30 06:00:19 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(NoteApp());
+}
+class NoteApp extends StatelessWidget {
+  const NoteApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Note APP",
+      home: Scaffold(),
+    );
+  }
+}
