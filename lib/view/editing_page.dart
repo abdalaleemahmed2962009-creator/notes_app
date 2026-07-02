88246c4015297fe0898e68facae3c953c9f04e0a
Your Name <you@example.com>
2026-07-03 01:16:35 +0300

    Update main.dart

--- lib/view/editing_page.dart
+++ lib/view/editing_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:notes/widget/Custom_%20textfield.dart';
-import 'package:notes/widget/Custom_Countainer.dart';
+
 import 'package:notes/widget/custom_icon.dart';
+import 'package:notes/widget/edit_Note.dart';
+
 class EditingPage extends StatelessWidget {
   const EditingPage({super.key});
 
@@ -12,39 +12,9 @@ class EditingPage extends StatelessWidget {
       appBar: AppBar(
         automaticallyImplyLeading: false,
         title: Text("Edit Note"),
-        actions: [
-          CustomIcon(shapeicon: Icons.check),
-        ],
+        actions: [CustomIcon(shapeicon: Icons.check)],
       ),
-   body:  Container(
-     child: Padding(
-       padding: const EdgeInsets.symmetric(horizontal: 7,vertical: 9),
-       child: ListView(
-         children: [
-           Padding(
-             padding: const EdgeInsets.only(
-               top: 40,
-               bottom: 14,
-               left: 6,
-               right: 6,
-             ),
-             child: CustomTextfield(title: 'Title '),
-           ),
-           Padding(
-             padding: const EdgeInsets.only(bottom: 14, left: 6, right: 6),
-             child: CustomTextfield(
-
-               title: 'Content',
-               maxlines: 6 ,
-             ),
-           ),
-
-
-
-         ],
-       ),
-     ),
-   )
+      body: EditNote(),
     );
   }
 }
