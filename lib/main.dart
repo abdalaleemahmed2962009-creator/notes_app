41b3d50b12ab8a2f83eb40531f3f6a8f061872c8
Your Name <you@example.com>
2026-07-03 05:13:39 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:notes/view/Home_Page.dart';
 
-void main(){
+void main()async{
   runApp(NoteApp());
+  await Hive.initFlutter();
+ await Hive.openBox("notes box");
 }
 class NoteApp extends StatelessWidget {
   const NoteApp({super.key});
